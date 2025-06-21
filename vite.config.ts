1d46d11d083ff1ab84007ab6778effa3d56795a8
rahul <rahul@gmail.com>
2025-06-21 19:22:23 +0530

    add email serive and toaster

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
+import flowbiteReact from 'flowbite-react/plugin/vite';
 import { defineConfig } from 'vite';
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
